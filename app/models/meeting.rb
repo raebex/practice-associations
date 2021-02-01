4da0551937b0894cf6120d88cfec3a27410233b3
rebecca <r.young1025@gmail.com>
2021-02-01 16:30:43 -0600

    add validations

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,6 @@
 class Meeting < ApplicationRecord
   has_many :speeches
   has_many :speakers, through: :speeches
+
+  validates :title, :agenda, :location, :time, presence: true
 end
--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,4 +1,6 @@
 class Speaker < ApplicationRecord
   has_many :speeches
   has_many :meetings, through: :speeches
+
+  validates :first_name, :last_name, :email, presence: true
 end
