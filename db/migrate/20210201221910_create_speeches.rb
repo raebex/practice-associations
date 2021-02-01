aec76fe4f790486692a0c07b7651636fb60c0aef
rebecca <r.young1025@gmail.com>
2021-02-01 16:26:45 -0600

    add has many associations to speaker and meeting models

---
+++ db/migrate/20210201221910_create_speeches.rb
@@ -0,0 +1,10 @@
+class CreateSpeeches < ActiveRecord::Migration[6.0]
+  def change
+    create_table :speeches do |t|
+      t.integer :speaker_id
+      t.integer :meeting_id
+
+      t.timestamps
+    end
+  end
+end
